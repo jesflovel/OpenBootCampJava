2d6bdc48d802c34eefd98cbbd4f6e30e8448c518
DESKTOP-Q89H8B6\Usuario <jesflovel@gmail.com>
2022-10-14 17:02:05 -0500

    Se revisa team de operadores

---
+++ Operadores/src/com/company/Operadores.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Operadores {
+    public static void main(String[] args) {
+        //Operadores
+
+        //Aritmeticos
+
+        // + - / * %
+
+        int number = 1;
+        int number2 = 2;
+
+        int resultado1= number + number2;
+        int resultado2= number - number2;
+        int resultado3= number * number2;
+        double resultado4= number / number2;
+
+        //Logicos, relacion, comparacion, booleanos
+
+        /*
+        >
+        >=
+        <
+        <=
+        ==
+        !=
+        &&
+        ||
+        * */
+
+        //Asignacion
+        /*
+        =
+        +=
+        -=
+        /=
+        *=
+        %=
+         */
+
+        //Incremento
+        // ++
+
+        //Decremento
+        // --
+
+    }
+}
