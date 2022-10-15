dddf25119c2c53674245147c84c5215605b3e25f
DESKTOP-Q89H8B6\Usuario <jesflovel@gmail.com>
2022-10-14 20:13:07 -0500

    Se revisa tema de ambito y retorno de una funcion

--- PalabrasReservadas/src/com/company/Funciones.java
+++ PalabrasReservadas/src/com/company/Funciones.java
@@ -6,21 +6,28 @@ public class Funciones {
         holaMundo("Jesus");
         String hola = devolverHola();
         System.out.println(hola);
+        int res = sum(5,5);
+        System.out.println(res);
     }
 
     //Función sin parámetros de entrada
-    private static void holaMundo() {
+    //Funcion con modificador "protected" el cual deja disponible la función solo para funciones del mismo paquete "com.company"
+    protected static void holaMundo() {
         System.out.println("Hola Mundo");
     }
 
     //Función con parámetros de entrada
-    private static void holaMundo(String nombre) {
+    public static void holaMundo(String nombre) {
         System.out.println("Hola " + nombre);
     }
 
     //Función con return
     private static String devolverHola(){
         return "Devolviendo Hola Mundo";
+    }
+
+    private static int sum(int num1, int num2){
+        return num1+ num2;
     }
 
 }
