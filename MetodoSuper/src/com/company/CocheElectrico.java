6ee44224a23232058dc9756896fe8646d8a1d90c
DESKTOP-Q89H8B6\Usuario <jesflovel@gmail.com>
2022-10-17 21:19:07 -0500

    Se revisa tema de metodo super

---
+++ MetodoSuper/src/com/company/CocheElectrico.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class CocheElectrico extends Coche{
+    String motorElectrico;
+
+    public CocheElectrico(){
+
+    }
+
+    public CocheElectrico(String motorElectrico){
+        this.motorElectrico = motorElectrico;
+    }
+
+    public CocheElectrico (String color, String marca, String modelo, String motorElectrico){
+        super(color, marca, modelo);
+        this.motorElectrico = motorElectrico;
+    }
+
+    @Override
+    public String toString() {
+        return "CocheElectrico{" +
+                "motorElectrico='" + motorElectrico + '\'' +
+                ", color='" + color + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", velocidad=" + velocidad +
+                '}';
+    }
+}
