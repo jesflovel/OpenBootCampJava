3cfce310a214f6c43b8f548372de35f00f98cf7c
DESKTOP-Q89H8B6\Usuario <jesflovel@gmail.com>
2022-10-18 23:58:46 -0500

    Se revisan temas de Listas y Mapas

--- MetodosString/src/com/company/Coche.java
+++ MetodosString/src/com/company/Coche.java
@@ -1,4 +1,16 @@
 package com.company;
 
 public class Coche {
+    String nombre = "Lambo";
+
+    public Coche(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche{" +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
 }
